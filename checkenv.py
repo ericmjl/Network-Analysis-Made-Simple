34a540cdf6c76a56fbfe01d69eb4c39e1eced43c
Eric Ma <ericmajinglong@gmail.com>
2015-11-08 20:52:41 -0500

    modified checkenv to alert user to install package
    Former-commit-id: 932f318fdd653d422d66bcae0a9de6310717f5a1

--- checkenv.py
+++ checkenv.py
@@ -6,8 +6,9 @@ def check_import(packagename):
     if packagename in (name for _, name, _ in iter_modules()):
         print('{0} exists.'.format(packagename))
     else:
-        print('Installing {0}...'.format(packagename))
-        os.system('conda install {0}'.format(p))
+        print('Please install {0}.'.format(packagename))
+        # print('Installing {0}...'.format(packagename))
+        # os.system('conda install {0}'.format(p))
 
 packages = ['networkx', 'numpy', 'matplotlib', 'circos', 'hiveplot', 'pandas']
 
