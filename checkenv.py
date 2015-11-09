0c67d95e75f364906eb11530bbff8109fa97c76c
Eric Ma <ericmajinglong@gmail.com>
2015-11-08 20:50:47 -0500

    added script to check that environment is good
    Former-commit-id: f637a3e222f991f92e579b493f6b241831045059

---
+++ checkenv.py
@@ -0,0 +1,15 @@
+# Check that the packages are installed.
+import os
+from pkgutil import iter_modules
+
+def check_import(packagename):
+    if packagename in (name for _, name, _ in iter_modules()):
+        print('{0} exists.'.format(packagename))
+    else:
+        print('Installing {0}...'.format(packagename))
+        os.system('conda install {0}'.format(p))
+
+packages = ['networkx', 'numpy', 'matplotlib', 'circos', 'hiveplot', 'pandas']
+
+for p in packages:
+    check_import(p)
