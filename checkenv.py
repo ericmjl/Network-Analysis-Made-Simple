e84b7a26dce6896eb3cf8fabffa3bcc9aecdb143
Eric J. Ma <ericmajinglong@gmail.com>
2016-05-08 11:27:40 -0400

    added jupyter and python 3 checking
    Former-commit-id: d90ce5b8d22ba52ffd1d8947d53f9d44379e427c

--- checkenv.py
+++ checkenv.py
@@ -1,6 +1,7 @@
 # Check that the packages are installed.
-import os
 from pkgutil import iter_modules
+import sys
+
 
 def check_import(packagename):
     if packagename in (name for _, name, _ in iter_modules()):
@@ -8,7 +9,10 @@ def check_import(packagename):
     else:
         return False
 
-packages = ['networkx', 'numpy', 'matplotlib', 'circos', 'hiveplot', 'pandas']
+packages = ['networkx', 'numpy', 'matplotlib', 'circos', 'hiveplot', 'pandas',
+            'jupyter']
 
 for p in packages:
     assert check_import(p), '{0} not present. Please install via pip or conda.'.format(p)
+
+assert sys.version_info.major >= 3, 'Please install Python 3!'
