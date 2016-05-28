6bfe3f54798c982845c2d70d3e98308641e26527
Eric J. Ma <ericmajinglong@gmail.com>
2016-05-28 16:13:17 -0700

    Added status report for passing environment check.

--- checkenv.py
+++ checkenv.py
@@ -12,7 +12,14 @@ def check_import(packagename):
 packages = ['networkx', 'numpy', 'matplotlib', 'circos', 'hiveplot', 'pandas',
             'jupyter']
 
+all_passed = True
+
 for p in packages:
-    assert check_import(p), '{0} not present. Please install via pip or conda.'.format(p)
+    assert check_import(p),\
+        '{0} not present. Please install via pip or conda.'.format(p)
 
 assert sys.version_info.major >= 3, 'Please install Python 3!'
+
+# Credit: @zmilicc for requesting this.
+if all_passed:
+    print('All checks passed. Your environment is good to go!')
