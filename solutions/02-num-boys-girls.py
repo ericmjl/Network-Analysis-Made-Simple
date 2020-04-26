0f8e2ef2f267dc17d7d180825d62e0a7f46a51b5
Eric Ma <ericmajinglong@gmail.com>
2020-04-26 00:06:43 -0400

    finished chapter 1
    Former-commit-id: 3211f2a7d2453e5f7fc4d2fb948c65f11ae39c48

--- solutions/02-num-boys-girls.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-mf_counts = Counter([d['gender']
-                     for n, d in G.nodes(data=True)])
-
-def test_answer(mf_counts):
-    assert mf_counts['female'] == 17
-    assert mf_counts['male'] == 12
-
-test_answer(mf_counts)
