0e971451795266ea910ffeabb14a9f79d8e110ad
Eric Ma <ericmajinglong@gmail.com>
2020-04-25 20:39:29 -0400

    committing data before moving to another computer
    Former-commit-id: 20a91bf69b0a50b7ce023cdd5302926915ac6198

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="nams",
+    # mandatory
+    version="0.1",
+    # mandatory     
+    author="Eric J. Ma, Mridul Seth",
+    packages=find_packages(),
+)
