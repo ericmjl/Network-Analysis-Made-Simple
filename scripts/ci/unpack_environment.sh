9c2f1ecace47616c3e90170f4278dc2db0f37041
Eric Ma <ericmajinglong@gmail.com>
2020-12-24 12:56:01 -0500

    Added unpacking of environment using conda-unpack

--- scripts/ci/unpack_environment.sh
+++ scripts/ci/unpack_environment.sh
@@ -1,2 +1,7 @@
 mkdir -p nams
 tar -xzf nams.tar.gz -C nams
+source nams/bin/activate
+conda-unpack
+source nams/bin/deactivate
+# A check -- just to make sure nams is installed
+conda env list | grep nams
