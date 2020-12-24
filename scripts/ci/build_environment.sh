19bd73b13be544205dfea61dd697a85def89c368
Eric Ma <ericmajinglong@gmail.com>
2020-12-24 12:39:49 -0500

    Switch environment build to mamba instead of conda

--- scripts/ci/build_environment.sh
+++ scripts/ci/build_environment.sh
@@ -1,5 +1,6 @@
 # Build conda environment.
-conda env create -f environment.yml
+conda install -c conda-forge mamba
+mamba env create -f environment.yml
 source activate nams
 python -m ipykernel install --user --name nams
 pip install .
