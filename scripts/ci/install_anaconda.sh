2e33c1180245107f64a989854771bef3ff5e09a2
Eric Ma <ericmajinglong@gmail.com>
2020-12-24 09:57:22 -0500

    Add script to install anaconda in CI system

---
+++ scripts/ci/install_anaconda.sh
@@ -0,0 +1,6 @@
+# Script to install anaconda, when needed.
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+conda config --set always_yes yes --set changeps1 no
+conda config --add channels conda-forge
