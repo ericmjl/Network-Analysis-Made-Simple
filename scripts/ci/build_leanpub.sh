eb4d3d2d795b53aef8bf1e4d37095ca87dad119d
Eric Ma <ericmajinglong@gmail.com>
2020-12-24 09:30:10 -0500

    Eliminated deploy step from Azure pipelines

--- scripts/ci/build_leanpub.sh
+++
@@ -1,4 +0,0 @@
-# Build LeanPub files
-source activate nams
-jupyter nbconvert --to python scripts/bookbuilder/markua.ipynb
-python scripts/bookbuilder/markua.py
--- scripts/ci/build_website.sh
+++
@@ -1,2 +0,0 @@
-source activate nams
-mkdocs build
