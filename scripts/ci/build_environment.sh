62d503ebda331adb089b16772070a20a5bf9abca
Eric Ma <ericmajinglong@gmail.com>
2020-12-24 13:47:28 -0500

    [INF] Move pip installable things out of environment spec file

--- scripts/ci/build_environment.sh
+++ scripts/ci/build_environment.sh
@@ -2,6 +2,8 @@
 conda install -c conda-forge mamba
 mamba env create -f environment.yml
 source activate nams
+pip install --no-deps mknotebooks
+pip install --no-deps .
 python -m ipykernel install --user --name nams
 pip install .
 conda install conda-pack
