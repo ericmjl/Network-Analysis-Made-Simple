4da7806d986b1a95e0986e2ff7ce5dfa7d6e139b
Eric Ma <ericmajinglong@gmail.com>
2020-12-25 11:55:33 -0500

    [INF] Add debugging statements to website and leanpub builds

--- scripts/ci/build_leanpub.sh
+++ scripts/ci/build_leanpub.sh
@@ -1,5 +1,6 @@
 # Build LeanPub book
 source nams_env/bin/activate || conda activate nams || source activate nams
+echo `which python`  # debugging statement
 python -m ipykernel install --user --name nams
 jupyter nbconvert --to python scripts/bookbuilder/markua.ipynb
 python scripts/bookbuilder/markua.py
--- scripts/ci/build_website.sh
+++ scripts/ci/build_website.sh
@@ -1,4 +1,5 @@
 # Build MkDocs website
 source nams_env/bin/activate || conda activate nams || source activate nams
+echo `which python`  # debugging statement
 python -m ipykernel install --user --name nams
 mkdocs build
