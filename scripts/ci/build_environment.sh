fac8ecbb7ceaef49f6350e2171b726c0a7a16656
Eric Ma <ericmajinglong@gmail.com>
2020-12-25 11:54:35 -0500

    [INF] Fixing build_environment.sh

--- scripts/ci/build_environment.sh
+++ scripts/ci/build_environment.sh
@@ -1,9 +1,9 @@
 # Build conda environment.
 conda install -c conda-forge mamba
-mamba env create -f environment.yml
-source activate nams
-pip install --no-deps mknotebooks
-pip install --no-deps .
-pip install .
+conda env create -f environment.yml
+conda activate nams || source activate nams
+python -m pip install --no-deps mknotebooks
+python -m pip install --no-deps .
+python -m pip install .
 conda install conda-pack
 conda pack -n nams -o nams.tar.gz
