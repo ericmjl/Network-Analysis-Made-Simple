dfbbb8ada52ff5f0b8fa1a3c75875eb3cdba6c8f
Mridul Seth <seth.mridul@gmail.com>
2021-03-22 13:38:02 +0100

    Use lab interface for binder links (#217)
    * Use lab interface for binder links
    
    * use current main branch of mknotebooks
    
    * use current main branch of mknotebooks

--- scripts/ci/build_environment.sh
+++ scripts/ci/build_environment.sh
@@ -2,7 +2,7 @@
 conda install -c conda-forge mamba
 conda env create -f environment.yml
 conda activate nams || source activate nams
-python -m pip install --no-deps mknotebooks
+python -m pip install --no-deps git+https://github.com/greenape/mknotebooks.git
 python -m pip install --no-deps .
 python -m pip install .
 conda install conda-pack
