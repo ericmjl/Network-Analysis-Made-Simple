3128638ea8fb8c4c0ad793aeea3c579de7df565a
Mridul Seth <seth.mridul@gmail.com>
2023-01-18 18:54:18 +0300

    explicity use mamba in build step

--- scripts/ci/build_environment.sh
+++ scripts/ci/build_environment.sh
@@ -1,9 +1,10 @@
 # Build conda environment.
 conda update -n base -c defaults conda
 conda install -c conda-forge mamba
-conda env create -f environment.yml
-conda activate nams || source activate nams
+mamba env create -f environment.yml
+mamba activate nams || mamba activate nams
 python -m pip install --no-deps .
 python -m pip install .
+conda list
 conda install conda-pack
 conda pack -n nams -o nams.tar.gz
