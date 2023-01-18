17cc917eb224529dfabc91b0ae73887727516e5f
Mridul Seth <seth.mridul@gmail.com>
2023-01-18 18:35:54 +0300

    Fixing up NAMS for NX 3.0

--- scripts/ci/build_environment.sh
+++ scripts/ci/build_environment.sh
@@ -1,8 +1,8 @@
 # Build conda environment.
+conda update -n base -c defaults conda
 conda install -c conda-forge mamba
 conda env create -f environment.yml
 conda activate nams || source activate nams
-python -m pip install --no-deps git+https://github.com/greenape/mknotebooks.git
 python -m pip install --no-deps .
 python -m pip install .
 conda install conda-pack
