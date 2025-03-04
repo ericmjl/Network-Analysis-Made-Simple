f03ece84bf00b2285a3b7f43e8fe922f9d520ef9
Eric Ma <ericmajinglong@gmail.com>
2025-03-03 20:06:50 -0500

    chore(ci)🛠️: Comment out Leanpub deployment steps and remove website build script
    - Commented out the Leanpub deployment steps in the GitHub Actions workflow.
    - Deleted the script for building the MkDocs website as it is no longer required.

--- scripts/ci/build_website.sh
+++
@@ -1,5 +0,0 @@
-# Build MkDocs website
-source nams_env/bin/activate || conda activate nams || source activate nams
-echo `which python`  # debugging statement
-python -m ipykernel install --user --name nams
-mkdocs build
