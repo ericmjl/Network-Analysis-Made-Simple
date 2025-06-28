4c3bf9a5664e73e5e3b86cd3023ad55aab8ab692
Eric Ma <ericmajinglong@gmail.com>
2025-06-28 12:15:13 -0400

    style(notebooks/03-practical/01-io)🎨: reformat markdown cells for consistent indentation and code style
    - Updated all markdown cell strings to remove excessive indentation and ensure consistent formatting.
    - Changed all markdown cell triple-quoted strings to be left-aligned for improved readability and maintainability.
    - Set hide_code=True for the first app.cell decorator for better notebook presentation.
    - Bumped __generated_with version from 0.13.0 to 0.14.9.

--- notebooks/03-practical/01-io.py
+++ notebooks/03-practical/01-io.py
@@ -22,11 +22,11 @@
 
 import marimo
 
-__generated_with = "0.13.0"
+__generated_with = "0.14.9"
 app = marimo.App()
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _():
     import warnings
 
@@ -52,100 +52,100 @@ def _():
 def _(mo):
     mo.md(
         r"""
-        In order to get you familiar with graph ideas,
-        I have deliberately chosen to steer away from
-        the more pedantic matters
-        of loading graph data to and from disk.
-        That said, the following scenario will eventually happen,
-        where a graph dataset lands on your lap,
-        and you'll need to load it in memory
-        and start analyzing it.
+    In order to get you familiar with graph ideas,
+    I have deliberately chosen to steer away from
+    the more pedantic matters
+    of loading graph data to and from disk.
+    That said, the following scenario will eventually happen,
+    where a graph dataset lands on your lap,
+    and you'll need to load it in memory
+    and start analyzing it.
 
-        Thus, we're going to go through graph I/O,
-        specifically the APIs on how to convert
-        graph data that comes to you
-        into that magical NetworkX object `G`.
+    Thus, we're going to go through graph I/O,
+    specifically the APIs on how to convert
+    graph data that comes to you
+    into that magical NetworkX object `G`.
 
-        Let's get going!
+    Let's get going!
 
-        ## Graph Data as Tables
+    ## Graph Data as Tables
 
-        Let's recall what we've learned in the introductory chapters.
-        Graphs can be represented using two **sets**:
+    Let's recall what we've learned in the introductory chapters.
+    Graphs can be represented using two **sets**:
 
-        - Node set
-        - Edge set
+    - Node set
+    - Edge set
 
-        ### Node set as tables
+    ### Node set as tables
 
-        Let's say we had a graph with 3 nodes in it: `A, B, C`.
-        We could represent it in plain text, computer-readable format:
+    Let's say we had a graph with 3 nodes in it: `A, B, C`.
+    We could represent it in plain text, computer-readable format:
 
-        ```csv
-        A
-        B
-        C
-        ```
+    ```csv
+    A
+    B
+    C
+    ```
 
-        Suppose the nodes also had metadata.
-        Then, we could tag on metadata as well:
+    Suppose the nodes also had metadata.
+    Then, we could tag on metadata as well:
 
-        ```csv
-        A, circle, 5
-        B, circle, 7
-        C, square, 9
-        ```
+    ```csv
+    A, circle, 5
+    B, circle, 7
+    C, square, 9
+    ```
 
-        Does this look familiar to you?
-        Yes, node sets can be stored in CSV format,
-        with one of the columns being node ID,
-        and the rest of the columns being metadata.
+    Does this look familiar to you?
+    Yes, node sets can be stored in CSV format,
+    with one of the columns being node ID,
+    and the rest of the columns being metadata.
 
-        ### Edge set as tables
+    ### Edge set as tables
 
-        If, between the nodes, we had 4 edges (this is a directed graph),
-        we can also represent those edges in plain text, computer-readable format:
+    If, between the nodes, we had 4 edges (this is a directed graph),
+    we can also represent those edges in plain text, computer-readable format:
 
-        ```csv
-        A, C
-        B, C
-        A, B
-        C, A
-        ```
+    ```csv
+    A, C
+    B, C
+    A, B
+    C, A
+    ```
 
-        And let's say we also had other metadata,
-        we can represent it in the same CSV format:
+    And let's say we also had other metadata,
+    we can represent it in the same CSV format:
 
-        ```csv
-        A, C, red
-        B, C, orange
-        A, B, yellow
-        C, A, green
-        ```
+    ```csv
+    A, C, red
+    B, C, orange
+    A, B, yellow
+    C, A, green
+    ```
 
-        If you've been in the data world for a while,
-        this should not look foreign to you.
-        Yes, edge sets can be stored in CSV format too!
-        Two of the columns represent the nodes involved in an edge,
-        and the rest of the columns represent the metadata.
+    If you've been in the data world for a while,
+    this should not look foreign to you.
+    Yes, edge sets can be stored in CSV format too!
+    Two of the columns represent the nodes involved in an edge,
+    and the rest of the columns represent the metadata.
 
-        ### Combined Representation
+    ### Combined Representation
 
-        In fact, one might also choose to combine
-        the node set and edge set tables together in a merged format:
+    In fact, one might also choose to combine
+    the node set and edge set tables together in a merged format:
 
-        ```
-        n1, n2, colour, shape1, num1, shape2, num2
-        A,  C,  red,    circle, 5,    square, 9
-        B,  C,  orange, circle, 7,    square, 9
-        A,  B,  yellow, circle, 5,    circle, 7
-        C,  A,  green,  square, 9,    circle, 5
-        ```
+    ```
+    n1, n2, colour, shape1, num1, shape2, num2
+    A,  C,  red,    circle, 5,    square, 9
+    B,  C,  orange, circle, 7,    square, 9
+    A,  B,  yellow, circle, 5,    circle, 7
+    C,  A,  green,  square, 9,    circle, 5
+    ```
 
-        In this chapter, the datasets that we will be looking at
-        are going to be formatted in both ways.
-        Let's get going.
-        """
+    In this chapter, the datasets that we will be looking at
+    are going to be formatted in both ways.
+    Let's get going.
+    """
     )
     return
 
@@ -154,18 +154,18 @@ def _(mo):
 def _(mo):
     mo.md(
         r"""
-        ## Dataset
+    ## Dataset
 
-        We will be working with the Divvy bike sharing dataset.
+    We will be working with the Divvy bike sharing dataset.
 
-        > Divvy is a bike sharing service in Chicago.
-        > Since 2013, Divvy has released their bike sharing dataset to the public.
-        > The 2013 dataset is comprised of two files:
-        > - `Divvy_Stations_2013.csv`, containing the stations in the system, and
-        > - `DivvyTrips_2013.csv`, containing the trips.
+    > Divvy is a bike sharing service in Chicago.
+    > Since 2013, Divvy has released their bike sharing dataset to the public.
+    > The 2013 dataset is comprised of two files:
+    > - `Divvy_Stations_2013.csv`, containing the stations in the system, and
+    > - `DivvyTrips_2013.csv`, containing the trips.
 
-        Let's dig into the data!
-        """
+    Let's dig into the data!
+    """
     )
     return
 
@@ -201,10 +201,10 @@ def _():
 def _(mo):
     mo.md(
         r"""
-        Now, let's load in both tables.
+    Now, let's load in both tables.
 
-        First is the `stations` table:
-        """
+    First is the `stations` table:
+    """
     )
     return
 
@@ -268,25 +268,25 @@ def _(trips_summary):
 def _(mo):
     mo.md(
         r"""
-        ## Graph Model
+    ## Graph Model
 
-        Given the data, if we wished to use a graph as a data model
-        for the number of trips between stations,
-        then naturally, nodes would be the stations,
-        and edges would be trips between them.
+    Given the data, if we wished to use a graph as a data model
+    for the number of trips between stations,
+    then naturally, nodes would be the stations,
+    and edges would be trips between them.
 
-        This graph would be directed,
-        as one could have more trips from station A to B
-        and less in the reverse.
+    This graph would be directed,
+    as one could have more trips from station A to B
+    and less in the reverse.
 
-        With this definition,
-        we can begin graph construction!
+    With this definition,
+    we can begin graph construction!
 
-        ### Create NetworkX graph from pandas edgelist
+    ### Create NetworkX graph from pandas edgelist
 
-        NetworkX provides an extremely convenient way
-        to load data from a pandas DataFrame:
-        """
+    NetworkX provides an extremely convenient way
+    to load data from a pandas DataFrame:
+    """
     )
     return
 
@@ -309,11 +309,11 @@ def _(trips_summary):
 def _(mo):
     mo.md(
         r"""
-        ### Inspect the graph
+    ### Inspect the graph
 
-        Once the graph is in memory,
-        we can inspect it to get out summary graph statistics.
-        """
+    Once the graph is in memory,
+    we can inspect it to get out summary graph statistics.
+    """
     )
     return
 
@@ -326,9 +326,7 @@ def _(G):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""You'll notice that the edge metadata have been added correctly: we have recorded in there the number of trips between stations."""
-    )
+    mo.md(r"""You'll notice that the edge metadata have been added correctly: we have recorded in there the number of trips between stations.""")
     return
 
 
@@ -354,17 +352,17 @@ def _(G):
 def _(mo):
     mo.md(
         r"""
-        ### Annotate node metadata
+    ### Annotate node metadata
 
-        We have rich station data on hand,
-        such as the longitude and latitude of each station,
-        and it would be a pity to discard it,
-        especially when we can potentially use it as part of the analysis
-        or for visualization purposes.
-        Let's see how we can add this information in.
+    We have rich station data on hand,
+    such as the longitude and latitude of each station,
+    and it would be a pity to discard it,
+    especially when we can potentially use it as part of the analysis
+    or for visualization purposes.
+    Let's see how we can add this information in.
 
-        Firstly, recall what the `stations` dataframe looked like:
-        """
+    Firstly, recall what the `stations` dataframe looked like:
+    """
     )
     return
 
@@ -379,15 +377,15 @@ def _(stations):
 def _(mo):
     mo.md(
         r"""
-        The `id` column gives us the node ID in the graph,
-        so if we set `id` to be the index,
-        if we then also loop over each row,
-        we can treat the rest of the columns as dictionary keys
-        and values as dictionary values,
-        and add the information into the graph.
+    The `id` column gives us the node ID in the graph,
+    so if we set `id` to be the index,
+    if we then also loop over each row,
+    we can treat the rest of the columns as dictionary keys
+    and values as dictionary values,
+    and add the information into the graph.
 
-        Let's see this in action.
-        """
+    Let's see this in action.
+    """
     )
     return
 
@@ -416,33 +414,33 @@ def _(G):
 def _(mo):
     mo.md(
         r"""
-        In `nxviz`, a `GeoPlot` object is available
-        that allows you to quickly visualize
-        a graph that has geographic data.
-        However, being `matplotlib`-based,
-        it is going to be quickly overwhelmed
-        by the sheer number of edges.
+    In `nxviz`, a `GeoPlot` object is available
+    that allows you to quickly visualize
+    a graph that has geographic data.
+    However, being `matplotlib`-based,
+    it is going to be quickly overwhelmed
+    by the sheer number of edges.
 
-        As such, we are going to first filter the edges.
+    As such, we are going to first filter the edges.
 
-        ### Exercise: Filter graph edges
+    ### Exercise: Filter graph edges
 
-        > Leveraging what you know about how to manipulate graphs,
-        > now try _filtering_ edges.
-        >
+    > Leveraging what you know about how to manipulate graphs,
+    > now try _filtering_ edges.
+    >
 
-        _Hint: NetworkX graph objects can be deep-copied using `G.copy()`:_
+    _Hint: NetworkX graph objects can be deep-copied using `G.copy()`:_
 
-        ```python
-        G_copy = G.copy()
-        ```
+    ```python
+    G_copy = G.copy()
+    ```
 
-        _Hint: NetworkX graph objects also let you remove edges:_
+    _Hint: NetworkX graph objects also let you remove edges:_
 
-        ```python
-        G.remove_edge(node1, node2)  # does not return anything
-        ```
-        """
+    ```python
+    G.remove_edge(node1, node2)  # does not return anything
+    ```
+    """
     )
     return
 
@@ -471,11 +469,11 @@ def _(G, G_________, ___, ____, ___________, d):
 def _(mo):
     mo.md(
         r"""
-        ### Visualize using GeoPlot
+    ### Visualize using GeoPlot
 
-        `nxviz` provides a GeoPlot object
-        that lets you quickly visualize geospatial graph data.
-        """
+    `nxviz` provides a GeoPlot object
+    that lets you quickly visualize geospatial graph data.
+    """
     )
     return
 
@@ -484,16 +482,16 @@ def _(mo):
 def _(mo):
     mo.md(
         r"""
-        A note on geospatial visualizations:
+    A note on geospatial visualizations:
 
-        > As the creator of `nxviz`,
-        > I would recommend using proper geospatial packages
-        > to build custom geospatial graph viz,
-        > such as [`pysal`](http://pysal.org/).)
-        >
-        > That said, `nxviz` can probably do what you need
-        > for a quick-and-dirty view of the data.
-        """
+    > As the creator of `nxviz`,
+    > I would recommend using proper geospatial packages
+    > to build custom geospatial graph viz,
+    > such as [`pysal`](http://pysal.org/).)
+    >
+    > That said, `nxviz` can probably do what you need
+    > for a quick-and-dirty view of the data.
+    """
     )
     return
 
@@ -512,12 +510,12 @@ def _(G_filtered):
 def _(mo):
     mo.md(
         r"""
-        Does that look familiar to you? Looks quite a bit like Chicago, I'd say :)
+    Does that look familiar to you? Looks quite a bit like Chicago, I'd say :)
 
-        Jesting aside, this visualization does help illustrate
-        that the majority of trips occur between stations that are
-        near the city center.
-        """
+    Jesting aside, this visualization does help illustrate
+    that the majority of trips occur between stations that are
+    near the city center.
+    """
     )
     return
 
@@ -526,13 +524,13 @@ def _(mo):
 def _(mo):
     mo.md(
         r"""
-        ## Pickling Graphs
+    ## Pickling Graphs
 
-        Since NetworkX graphs are Python objects,
-        the canonical way to save them is by pickling them.
+    Since NetworkX graphs are Python objects,
+    the canonical way to save them is by pickling them.
 
-        Here's an example in action:
-        """
+    Here's an example in action:
+    """
     )
     return
 
@@ -563,15 +561,15 @@ def _(pickle):
 def _(mo):
     mo.md(
         r"""
-        ### Exercise: checking graph integrity
+    ### Exercise: checking graph integrity
 
-        If you get a graph dataset as a pickle,
-        you should always check it against reference properties
-        to make sure of its data integrity.
+    If you get a graph dataset as a pickle,
+    you should always check it against reference properties
+    to make sure of its data integrity.
 
-        > Write a function that tests that the graph
-        > has the correct number of nodes and edges inside it.
-        """
+    > Write a function that tests that the graph
+    > has the correct number of nodes and edges inside it.
+    """
     )
     return
 
@@ -593,25 +591,25 @@ def _(G):
 def _(mo):
     mo.md(
         r"""
-        ## Other text formats
+    ## Other text formats
 
-        CSV files and `pandas` DataFrames
-        give us a convenient way to store graph data,
-        and if possible, do insist with your data collaborators
-        that they provide you with graph data that are in this format.
-        If they don't, however, no sweat!
-        After all, Python is super versatile.
+    CSV files and `pandas` DataFrames
+    give us a convenient way to store graph data,
+    and if possible, do insist with your data collaborators
+    that they provide you with graph data that are in this format.
+    If they don't, however, no sweat!
+    After all, Python is super versatile.
 
-        In this ebook, we have loaded data in
-        from non-CSV sources,
-        sometimes by parsing text files raw,
-        sometimes by treating special characters as delimiters in a CSV-like file,
-        and sometimes by resorting to parsing JSON.
+    In this ebook, we have loaded data in
+    from non-CSV sources,
+    sometimes by parsing text files raw,
+    sometimes by treating special characters as delimiters in a CSV-like file,
+    and sometimes by resorting to parsing JSON.
 
-        You can see other examples of how we load data
-        by browsing through the source file of `load_data.py`
-        and studying how we construct graph objects.
-        """
+    You can see other examples of how we load data
+    by browsing through the source file of `load_data.py`
+    and studying how we construct graph objects.
+    """
     )
     return
 
@@ -620,10 +618,10 @@ def _(mo):
 def _(mo):
     mo.md(
         r"""
-        ## Solutions
+    ## Solutions
 
-        The solutions to this chapter's exercises are below
-        """
+    The solutions to this chapter's exercises are below
+    """
     )
     return
 
