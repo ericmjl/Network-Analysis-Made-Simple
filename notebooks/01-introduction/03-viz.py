326ac83dc23292f105b71e5d5fe70c4a94bf3af4
Eric Ma <ericmajinglong@gmail.com>
2026-07-11 16:08:28 -0400

    style(notebooks): standardize app config and mo import cell conventions
    - All notebooks: app = marimo.App(width="medium", auto_download=["html"])
    - Standardized mo import cell to def _(): with hide_code=True
    - Removed bare 'mo' display expressions from import cells
    - Renamed mo_def/mo_import cell functions to _ for consistency

--- notebooks/01-introduction/03-viz.py
+++ notebooks/01-introduction/03-viz.py
@@ -1,11 +1,11 @@
 import marimo
 
 __generated_with = "0.23.9"
-app = marimo.App()
+app = marimo.App(width="medium", auto_download=["html"])
 
 
 @app.cell(hide_code=True)
-def mo_import():
+def _():
     import marimo as mo
 
     return (mo,)
--- notebooks/02-algorithms/01-hubs.py
+++ notebooks/02-algorithms/01-hubs.py
@@ -1,7 +1,7 @@
 import marimo
 
 __generated_with = "0.23.9"
-app = marimo.App()
+app = marimo.App(width="medium", auto_download=["html"])
 
 
 @app.cell(hide_code=True)
